29eec1da65149482cff33a093ed1a0ee7b40e790
prasea <prajanya.stha7@gmail.com>
2024-06-09 16:18:25 +0545

    Order created quotes using ordered scope in Quote model

--- quote-editor/app/controllers/quotes_controller.rb
+++ quote-editor/app/controllers/quotes_controller.rb
@@ -1,7 +1,7 @@
 class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
