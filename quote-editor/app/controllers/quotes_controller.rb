210e9e7ca176a4b9e745e751f45d69cc2b0c84f7
prasea <prajanya.stha7@gmail.com>
2024-06-09 16:12:00 +0545

    Use Turbo Frames and TURBO_STREAM to display quote form and prpend newly created quote

--- quote-editor/app/controllers/quotes_controller.rb
+++ quote-editor/app/controllers/quotes_controller.rb
@@ -14,7 +14,10 @@ class QuotesController < ApplicationController
   def create
     @quote = Quote.new(quote_params)
     if @quote.save
-      redirect_to quotes_path, notice: 'Quote created'
+      respond_to do|format|
+        format.html {redirect_to quotes_path, notice: 'Quote created'}
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
